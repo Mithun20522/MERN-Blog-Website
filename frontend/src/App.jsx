5e107551cc72315b57e7ed07ea81eb07e8c9441d
mithun20522 <mithun20522@iiitd.ac.in>
2024-01-22 17:01:57 +0530

    Pages routing done .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Projects from './pages/Projects'
+import Register from './pages/Register'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
