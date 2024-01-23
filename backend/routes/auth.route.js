3ecece73be78b23068d86b1add23653b640c06ca
mithun20522 <mithun20522@iiitd.ac.in>
2024-01-24 00:45:19 +0530

    Created the login api route and validation of a user.

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js';
+import { signup, signin } from '../controllers/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', signup);
+authRouter.post('/login', signin);
 
 export default authRouter;
