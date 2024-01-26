16e13d5f5e0788748eddc8c4868a8127f86b873f
mithun20522 <mithun20522@iiitd.ac.in>
2024-01-26 13:49:54 +0530

    sidebar for the dashboard completed.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useLocation} from 'react-router-dom'
+import DashSidebar from '../components/DashSidebar';
+import Dashprofile from '../components/Dashprofile';
 
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams =new URLSearchParams(location.search);
+    const tabFromURL = urlParams.get('tab');
+
+    if(tabFromURL) setTab(tabFromURL);
+
+  }, [location.search])
+
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        <DashSidebar/>
+      </div>
+      {tab === 'profile' && <Dashprofile/>}
+    </div>
   )
 }
 
