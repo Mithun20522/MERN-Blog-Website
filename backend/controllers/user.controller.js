b0eeeea9d0924854b8c29a1455e19a192b58f48c
mithun20522 <mithun20522@iiitd.ac.in>
2024-02-26 02:27:23 +0530

    added like a comment functionality.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -134,4 +134,6 @@ export const getUser = async (req, res, next) => {
       next(error);
     }
 }
+
+
   
--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { createComment, getComments } from '../controllers/comment.controller.js';
+import { createComment, getComments, likeComment } from '../controllers/comment.controller.js';
 import {verifyToken} from '../utils/verifyUser.js'
 const router = express.Router();
 router.post('/create-comment', verifyToken ,createComment);
-router.get('/get-comment/:postId', verifyToken, getComments);
+router.get('/get-comment/:postId', getComments);
+router.put('/like-comment/:commentId', verifyToken, likeComment)
 export default router;
