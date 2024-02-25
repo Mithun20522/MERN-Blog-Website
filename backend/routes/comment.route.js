2c8bd8292ed378e503f3c3f5bb62608e9ebc055c
mithun20522 <mithun20522@iiitd.ac.in>
2024-02-25 22:57:49 +0530

    show comments of a post functionality and UI done

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import { createComment } from '../controllers/comment.controller.js';
+import { createComment, getComments } from '../controllers/comment.controller.js';
 import {verifyToken} from '../utils/verifyUser.js'
 const router = express.Router();
 router.post('/create-comment', verifyToken ,createComment);
-
+router.get('/get-comment/:postId', verifyToken, getComments);
 export default router;
